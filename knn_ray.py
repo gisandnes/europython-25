c529e36bb272ebbfd9e420ccc774631e0d06cd3e
Jan Pipek <jan.pipek@gmail.com>
2025-07-05 11:49:41 +0200

    create_grid

--- knn_ray.py
+++ knn_ray.py
@@ -4,6 +4,11 @@ import ray
 def calculate_distances(query_points: np.ndarray, dataset: np.ndarray) -> np.ndarray:
     return np.sqrt(np.sum((dataset[:, np.newaxis, :] - query_points) ** 2, axis=-1))
 
+
+N_POINTS = 10
+LIMIT = 10
+
+
 @ray.remote
 def knn_search(
     query_points: np.ndarray,
@@ -11,11 +16,19 @@ def knn_search(
     k: int,
     distances_func=calculate_distances,
 ):
-    pass
+    distances = distances_func(query_points, dataset)
+    nearest_indices = np.argpartition(distances, k, axis=0)[:k].T
+    return nearest_indices
 
 
-def create_grid():
-    pass
+def create_grid() -> tuple[np.ndarray, ...]:
+    """
+    Create a homogenous grid of points to create a map.
+    """
+    # TODO: Add floor
+    x = np.linspace(-LIMIT, LIMIT, N_POINTS)
+    y = np.linspace(-LIMIT, LIMIT, N_POINTS)
+    return np.meshgrid(x, y)
 
 
 @ray.remote
@@ -27,8 +40,11 @@ def compute_prices(query_points, data_points):
 
 
 if __name__ == "___main__":
-    create_grid()
     ray.init()
+
+    x, y = create_grid()
+    data = np.hstack([x, y, 1])
+
     # Split the grid in batches
     # Compute prices & compose together
     #
